8fb1cd8ddc82d8f3283322aaa9a98b93dd6cd4fc
qiliang.huang <qiliang.huang@electrifai.com>
2023-10-17 13:38:05 +0800

    Balance_5

--- packages/RandomTree.py
+++ packages/RandomTree.py
@@ -17,13 +17,13 @@ def initial_tree(node_string):
     return bst
 
 def get_random_tree(n=20):
-    res = '  '.join( set([str(random.randint(0, 1000)) for x in range(n)]))
+    res = '  '.join( set([str(random.randint(0, 100000)) for x in range(n)]))
     return res
 
 
 
 if __name__ == '__main__':
-    N = 500
+    N = 10000
     node_string = get_random_tree(N) 
     print(node_string)
     print()
